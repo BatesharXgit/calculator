14003e007daddf837562e941416d9ed9aa262e5f
BatesharXgit <bateshar.xyz@gmail.com>
2023-09-13 02:33:45 +0530

    v 1.0.0

--- lib/unitConverter/converter.dart
+++ lib/unitConverter/converter.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class LengthConverterPage extends StatefulWidget {
   @override
@@ -13,15 +14,42 @@ class _LengthConverterPageState extends State<LengthConverterPage> {
   String selectedOutputUnit = 'Feet';
 
   Map<String, double> lengthUnits = {
-    'Meters': 1.0,
+    'Kilometers': 1.0,
+    'Meters': 1000.0,
+    'Miles': 0.000621371,
     'Feet': 3.28084,
     'Inches': 39.3701,
     'Yards': 1.09361,
-    'Kilometers': 0.001,
-    'Miles': 0.000621371,
-    'Centimeters': 100.0,
-    'Millimeters': 1000.0,
+    'Decimeters': 10000.0,
+    'Centimeters': 100000.0,
+    'Millimeters': 1000000.0,
+    'Micrometers': 1000000000.0,
+    'Nanometers': 1000000000000.0,
+    'Picometers': 1000000000000000.0,
+    'Light Years': 1.057e-13,
+    'Parsecs': 3.24078e-14,
+    'Lunar Distances': 6.685e6,
+    'Astronomical Units': 6.68459e-9,
   };
+
+  List<Map<String, String>> lengthUnitsWithAbbreviations = [
+    {'name': 'Kilometers', 'abbreviation': 'km'},
+    {'name': 'Meters', 'abbreviation': 'm'},
+    {'name': 'Miles', 'abbreviation': 'mi'},
+    {'name': 'Feet', 'abbreviation': 'ft'},
+    {'name': 'Inches', 'abbreviation': 'in'},
+    {'name': 'Yards', 'abbreviation': 'yd'},
+    {'name': 'Decimeters', 'abbreviation': 'dm'},
+    {'name': 'Centimeters', 'abbreviation': 'cm'},
+    {'name': 'Millimeters', 'abbreviation': 'mm'},
+    {'name': 'Micrometers', 'abbreviation': 'µm'},
+    {'name': 'Nanometers', 'abbreviation': 'nm'},
+    {'name': 'Picometers', 'abbreviation': 'pm'},
+    {'name': 'Light Years', 'abbreviation': 'ly'},
+    {'name': 'Parsecs', 'abbreviation': 'pc'},
+    {'name': 'Lunar Distances', 'abbreviation': 'LD'},
+    {'name': 'Astronomical Units', 'abbreviation': 'AU'},
+  ];
 
   TextEditingController inputController = TextEditingController();
 
@@ -53,13 +81,20 @@ class _LengthConverterPageState extends State<LengthConverterPage> {
 
   @override
   Widget build(BuildContext context) {
+    Color backgroundColor = Theme.of(context).colorScheme.background;
+
+    Color tertiaryColor = Theme.of(context).colorScheme.tertiary;
     return Scaffold(
       appBar: AppBar(
         elevation: 0,
-        backgroundColor: Colors.black,
+        backgroundColor: backgroundColor,
         centerTitle: true,
-        title: Text('Length Conversion'),
+        title: Text(
+          'Length Conversion',
+          style: TextStyle(color: tertiaryColor),
+        ),
       ),
+      backgroundColor: backgroundColor,
       body: Padding(
         padding: EdgeInsets.all(20.0),
         child: Column(
@@ -69,9 +104,10 @@ class _LengthConverterPageState extends State<LengthConverterPage> {
               alignment: Alignment.centerRight,
               child: Text(
                 '$outputValue $selectedOutputUnit',
-                style: TextStyle(
-                  fontSize: 18.0,
+                style: GoogleFonts.rubik(
+                  fontSize: 25.0,
                   fontWeight: FontWeight.bold,
+                  color: tertiaryColor,
                 ),
               ),
             ),
@@ -85,56 +121,69 @@ class _LengthConverterPageState extends State<LengthConverterPage> {
                   padding: EdgeInsets.symmetric(horizontal: 12.0),
                   decoration: BoxDecoration(
                     border: Border.all(
-                      color: Colors.grey,
+                      color: tertiaryColor,
                       width: 1.0,
                     ),
-                    borderRadius: BorderRadius.circular(8.0),
+                    borderRadius: BorderRadius.circular(20.0),
                   ),
-                  child: DropdownButton<String>(
-                    value: selectedInputUnit,
-                    onChanged: (value) {
-                      setState(() {
-                        selectedInputUnit = value!;
-                        convertLength();
-                      });
-                    },
-                    items: lengthUnits.keys.map((unit) {
-                      return DropdownMenuItem<String>(
-                        value: unit,
-                        child: Text(unit),
-                      );
-                    }).toList(),
-                    style: TextStyle(color: Colors.white),
-                    underline: Container(),
+                  child: Row(
+                    children: <Widget>[
+                      DropdownButton<String>(
+                        value: selectedInputUnit,
+                        onChanged: (value) {
+                          setState(() {
+                            selectedInputUnit = value!;
+                            convertLength();
+                          });
+                        },
+                        items: lengthUnitsWithAbbreviations.map((unit) {
+                          return DropdownMenuItem<String>(
+                            value: unit['name']!,
+                            child: Text(
+                                '${unit['name']} (${unit['abbreviation']})'),
+                          );
+                        }).toList(),
+                        style: TextStyle(color: tertiaryColor),
+                        underline: Container(),
+                      ),
+                    ],
                   ),
                 ),
-                Icon(Icons.arrow_downward, color: Colors.grey),
+                Icon(
+                  Icons.arrow_downward,
+                  color: tertiaryColor,
+                  size: 30,
+                ),
                 Container(
                   padding: EdgeInsets.symmetric(horizontal: 12.0),
                   decoration: BoxDecoration(
                     border: Border.all(
-                      color: Colors.grey,
+                      color: tertiaryColor,
                       width: 1.0,
                     ),
-                    borderRadius: BorderRadius.circular(8.0),
+                    borderRadius: BorderRadius.circular(20.0),
                   ),
-                  child: DropdownButton<String>(
-                    value: selectedOutputUnit,
-                    onChanged: (value) {
-                      setState(() {
-                        selectedOutputUnit = value!;
-                        convertLength();
-                      });
-                    },
-                    items: lengthUnits.keys.map((unit) {
-                      return DropdownMenuItem<String>(
-                        value: unit,
-                        child: Text(unit),
-                      );
-                    }).toList(),
-                    style:
-                        TextStyle(color: Colors.white), // Customize text color
-                    underline: Container(), // Remove default underline
+                  child: Row(
+                    children: <Widget>[
+                      DropdownButton<String>(
+                        value: selectedOutputUnit,
+                        onChanged: (value) {
+                          setState(() {
+                            selectedOutputUnit = value!;
+                            convertLength();
+                          });
+                        },
+                        items: lengthUnitsWithAbbreviations.map((unit) {
+                          return DropdownMenuItem<String>(
+                            value: unit['name']!,
+                            child: Text(
+                                '${unit['name']} (${unit['abbreviation']})'),
+                          );
+                        }).toList(),
+                        style: TextStyle(color: tertiaryColor),
+                        underline: Container(),
+                      ),
+                    ],
                   ),
                 ),
               ],
@@ -147,7 +196,9 @@ class _LengthConverterPageState extends State<LengthConverterPage> {
               keyboardType: TextInputType.number,
               decoration: InputDecoration(
                 labelText: 'Enter Length',
-                border: OutlineInputBorder(),
+                fillColor: tertiaryColor,
+                border:
+                    OutlineInputBorder(borderRadius: BorderRadius.circular(20)),
               ),
             ),
           ],
@@ -170,15 +221,24 @@ class _AreaConverterPageState extends State<AreaConverterPage> {
   String selectedOutputUnit = 'Square Feet';
 
   Map<String, double> areaUnits = {
-    'Square Meters': 1.0,
-    'Square Feet': 10.7639,
-    'Square Inches': 1550.0,
-    'Square Yards': 1.19599,
-    'Square Kilometers': 0.0001,
-    'Square Miles': 3.861e-8,
-    'Square Centimeters': 10000.0,
-    'Square Millimeters': 1000000.0,
+    'Square Kilometers': 1.0,
+    'Square Meters': 1000000.0,
+    'Square Miles': 0.386102,
+    'Square Feet': 10763910.4,
+    'Square Inches': 1550003100.0,
+    'Square Yards': 1195990.05,
+    // Add other area units and conversion factors as needed
   };
+
+  List<Map<String, String>> areaUnitsWithAbbreviations = [
+    {'name': 'Square Kilometers', 'abbreviation': 'km²'},
+    {'name': 'Square Meters', 'abbreviation': 'm²'},
+    {'name': 'Square Miles', 'abbreviation': 'mi²'},
+    {'name': 'Square Feet', 'abbreviation': 'ft²'},
+    {'name': 'Square Inches', 'abbreviation': 'in²'},
+    {'name': 'Square Yards', 'abbreviation': 'yd²'},
+    // Add other area units with abbreviations as needed
+  ];
 
   TextEditingController inputController = TextEditingController();
 
@@ -210,13 +270,20 @@ class _AreaConverterPageState extends State<AreaConverterPage> {
 
   @override
   Widget build(BuildContext context) {
+    Color backgroundColor = Theme.of(context).colorScheme.background;
+
+    Color tertiaryColor = Theme.of(context).colorScheme.tertiary;
     return Scaffold(
       appBar: AppBar(
         elevation: 0,
-        backgroundColor: Colors.black,
+        backgroundColor: backgroundColor,
         centerTitle: true,
-        title: Text('Area Conversion'),
+        title: Text(
+          'Area Conversion',
+          style: TextStyle(color: tertiaryColor),
+        ),
       ),
+      backgroundColor: backgroundColor,
       body: Padding(
         padding: EdgeInsets.all(20.0),
         child: Column(
@@ -227,8 +294,9 @@ class _AreaConverterPageState extends State<AreaConverterPage> {
               child: Text(
                 '$outputValue $selectedOutputUnit',
                 style: TextStyle(
-                  fontSize: 18.0,
+                  fontSize: 25.0,
                   fontWeight: FontWeight.bold,
+                  color: tertiaryColor,
                 ),
               ),
             ),
@@ -242,55 +310,69 @@ class _AreaConverterPageState extends State<AreaConverterPage> {
                   padding: EdgeInsets.symmetric(horizontal: 12.0),
                   decoration: BoxDecoration(
                     border: Border.all(
-                      color: Colors.grey,
+                      color: tertiaryColor,
                       width: 1.0,
                     ),
-                    borderRadius: BorderRadius.circular(8.0),
+                    borderRadius: BorderRadius.circular(20.0),
                   ),
-                  child: DropdownButton<String>(
-                    value: selectedInputUnit,
-                    onChanged: (value) {
-                      setState(() {
-                        selectedInputUnit = value!;
-                        convertArea();
-                      });
-                    },
-                    items: areaUnits.keys.map((unit) {
-                      return DropdownMenuItem<String>(
-                        value: unit,
-                        child: Text(unit),
-                      );
-                    }).toList(),
-                    style: TextStyle(color: Colors.white),
-                    underline: Container(),
+                  child: Row(
+                    children: <Widget>[
+                      DropdownButton<String>(
+                        value: selectedInputUnit,
+                        onChanged: (value) {
+                          setState(() {
+                            selectedInputUnit = value!;
+                            convertArea();
+                          });
+                        },
+                        items: areaUnitsWithAbbreviations.map((unit) {
+                          return DropdownMenuItem<String>(
+                            value: unit['name']!,
+                            child: Text(
+                                '${unit['name']} (${unit['abbreviation']})'),
+                          );
+                        }).toList(),
+                        style: TextStyle(color: tertiaryColor),
+                        underline: Container(),
+                      ),
+                    ],
                   ),
                 ),
-                Icon(Icons.arrow_downward, color: Colors.grey),
+                Icon(
+                  Icons.arrow_downward,
+                  color: tertiaryColor,
+                  size: 30,
+                ),
                 Container(
                   padding: EdgeInsets.symmetric(horizontal: 12.0),
                   decoration: BoxDecoration(
                     border: Border.all(
-                      color: Colors.grey,
+                      color: tertiaryColor,
                       width: 1.0,
                     ),
-                    borderRadius: BorderRadius.circular(8.0),
+                    borderRadius: BorderRadius.circular(20.0),
                   ),
-                  child: DropdownButton<String>(
-                    value: selectedOutputUnit,
-                    onChanged: (value) {
-                      setState(() {
-                        selectedOutputUnit = value!;
-                        convertArea();
-                      });
-                    },
-                    items: areaUnits.keys.map((unit) {
-                      return DropdownMenuItem<String>(
-                        value: unit,
-                        child: Text(unit),
-                      );
-                    }).toList(),
-                    style: TextStyle(color: Colors.white),
-                    underline: Container(),
+                  child: Row(
+                    children: <Widget>[
+                      DropdownButton<String>(
+                        value: selectedOutputUnit,
+                        onChanged: (value) {
+                          setState(() {
+                            selectedOutputUnit = value!;
+                            convertArea();
+                          });
+                        },
+                        items: areaUnitsWithAbbreviations.map((unit) {
+                          return DropdownMenuItem<String>(
+                            value: unit['name']!,
+                            child: Text(
+                                '${unit['name']} (${unit['abbreviation']})'),
+                          );
+                        }).toList(),
+                        style: TextStyle(color: tertiaryColor),
+                        underline: Container(),
+                      ),
+                    ],
                   ),
                 ),
               ],
@@ -303,7 +385,9 @@ class _AreaConverterPageState extends State<AreaConverterPage> {
               keyboardType: TextInputType.number,
               decoration: InputDecoration(
                 labelText: 'Enter Area',
-                border: OutlineInputBorder(),
+                fillColor: tertiaryColor,
+                border:
+                    OutlineInputBorder(borderRadius: BorderRadius.circular(20)),
               ),
             ),
           ],
