7a88faf663d622398a0017c852128db2430b115d
BatesharXgit <bateshar.xyz@gmail.com>
2023-09-14 23:47:22 +0530

    v 1.0.0

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,23 @@
 import 'package:calculator/homepage.dart';
 import 'package:calculator/themes/theme.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:get/get.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() => runApp(MyApp());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+  ]);
+
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
